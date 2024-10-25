878cb673a61e4647af779be4a3c0edcbadbad25a
Felix <felix@niederer.dev>
2024-10-25 21:53:20 +0200

    fix

--- flake.nix
+++ flake.nix
@@ -134,7 +134,7 @@
           security.pam.services.sudo.text = ''
             auth    sufficient    ${rust-fp-pam-module}/lib/libpam_sober.so
             account sufficient    ${rust-fp-pam-module}/lib/libpam_sober.so
-
+          '';
 
 
           
