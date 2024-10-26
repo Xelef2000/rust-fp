987432a82b012e51a11cf69689eb8abd887cdc7c
Felix <felix@niederer.dev>
2024-10-26 17:36:47 +0200

    su

--- flake.nix
+++ flake.nix
@@ -125,12 +125,16 @@
 
           # gdm-launch-environment runuser   
           # gdm-password goupdel  groupmod  
-          #  polkit-1 
+          #  polkit-1 sudo su login
 
-          # security.pam.services.polkit-1.text = ''
-          #   auth    sufficient    ${rust-fp-pam-module}/lib/libpam_sober.so
-          #   account sufficient    ${rust-fp-pam-module}/lib/libpam_sober.so
-          # '';
+          security.pam.services.su.text = ''
+            auth sufficient ${linux-pam}/lib/security/pam_rootok.so # rootok (order 10200)
+            auth required ${linux-pam}/lib/security/pam_faillock.so # faillock (order 10400)
+            auth sufficient ${rust-fp-pam-module}/lib/libpam_sober.so # fprintd (order 11300)
+            auth sufficient ${linux-pam}/lib/security/pam_unix.so likeauth try_first_pass # unix (order 11500)
+            auth required ${linux-pam}/lib/security/pam_deny.so # deny (order 12300)
+
+          '';
 
 
 
